cd1f579305dbbdfba460efee642cfeb4a49226df
harshwardhanfromIndia <harshwardhan5417@gmail.com>
2026-06-18 11:37:34 +0530

    Add change bg color solution

---
+++ level-1/changeBgColor.js
@@ -0,0 +1,5 @@
+// Problem: Change the background color of an element with class 'bg' to blue
+// Approach: Using querySelector to select the element and directly updating its style
+// Time: O(1) | Space: O(1)
+
+document.querySelector(".bg").style.backgroundColor = "blue";
